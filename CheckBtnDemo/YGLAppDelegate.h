95e90864ad8e46559d8515e74e4327d119ab0d09
suiwei <evenfate@gmail.com>
2014-01-02 11:45:50 +0800

    Initial Commit

---
+++ CheckBtnDemo/YGLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YGLAppDelegate.h
+//  CheckBtnDemo
+//
+//  Created by ygl10 on 14-1-2.
+//  Copyright (c) 2014年 ygl10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YGLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CheckBtnDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CheckBtnDemo
+//
+//  Created by ygl10 on 14-1-2.
+//  Copyright (c) 2014年 ygl10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YGLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YGLAppDelegate class]));
+    }
+}
